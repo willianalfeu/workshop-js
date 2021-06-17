119a2042353366ff22098c075136e57965c8d4af
Willian Alfeu <willianalfeu@gmail.com>
2021-06-17 18:28:31 -0300

    desenvolvimento durante o workshop

--- js/script.js
+++ js/script.js
@@ -1,85 +1,89 @@
+//getElementById
+//getElementsByClassName (array)
+//querySelector (elemento)
+//querySelectorAll (array)
+
+
+const regexEmail = /([a-z0-9_.]*\@([a-z]*)\.([a-z]{1,3})(\.[a-z]*)?)/g;
+//n esta funcionando
+const regexNumero = /[0-9]{1,7}([0-9]{0}?)/g;
+
 let form;
-function start() {
+function load(){
     form = document.getElementById("formCadastro");
-    form.onsubmit = validar;
-    //form.addEventListener("submit", validar);
+    form.onsubmit = validarFormulario;
+    //form.addEventListener("submit", validarFormulario);
 }
 
-function limparErros() {
-    let arrayErros = document.getElementsByClassName("erro");
-    while (arrayErros.length > 0) {
-        let element = arrayErros[0];
-        element.remove();
+function validarEmail(email){
+    return regexEmail.test(email);
+}
+
+function limparError(){
+    let arrayErro = document.getElementsByClassName("erro");
+    while(arrayErro.length>0){
+        arrayErro[0].remove();
     }
 }
 
-function validarEmail(email) {
-    const re = /([a-z0-9_.]*\@([a-z]*)\.([a-z]{1,3})(\.[a-z]*)?)/g;
-    return re.test(email);
-}
-
-//function validar(){
-let validar = function () {
-    let valido = true;
-    limparErros();
-
+let validarFormulario = function(){
+//function validarFormulario(){
+    let ehValido = true;
+    limparError();
+        
     let erro = document.createElement("p");
     erro.className = "erro";
-
-    //validacao text
-    let nome = document.getElementById("nome");
-    if (nome.value == "") {
-        valido = false;
+    
+    let nome = document.querySelector("#nome");
+    if(nome.value.length==0){
+        ehValido = false;
         erro.textContent = "nome inválido";
         nome.after(erro.cloneNode(true));
     }
 
-    let email = document.getElementById("email");
-    if (validarEmail(email.value.toLowerCase())==false) {
-        valido = false;
-        erro.textContent = "email inválido";
+    let email = document.querySelector("#email");
+    if(validarEmail(email.value.toLowerCase())==false){
+        ehValido = false;
+        erro.textContent = "e-mail inválido";
         email.after(erro.cloneNode(true));
     }
-
-    let celular = document.getElementById("celular");
-    if (celular.value == "") {
-        valido = false;
+    
+    let celular = document.querySelector("#celular");
+    if(regexNumero.test(celular.value.toLowerCase())==false){
+        ehValido = false;
         erro.textContent = "celular inválido";
         celular.after(erro.cloneNode(true));
     }
 
-    //validação select
-    let curso = document.getElementById("curso");
-    if (curso.value == "") {
-        valido = false;
+    let curso = document.querySelector("#curso");
+    if(curso.value == "0"){
+        ehValido = false;
         erro.textContent = "curso inválido";
         curso.after(erro.cloneNode(true));
     }
 
-    //validação radio 1
-    let manha = document.getElementById("manha");
-    let tarde = document.getElementById("tarde");
-    if (manha.checked == false && tarde.checked == false) {
-        valido = false;
-        erro.textContent = "horario inválido";
-        //manha.parentElement.append(erro.cloneNode(true));
+    /*
+    let opcaoOnline = document.querySelector("#online");
+    let opcaoPresencial = document.querySelector("#presencial");
+    if(opcaoOnline.checked==false && opcaoPresencial.checked==false ){
+        ehValido = false;
+        erro.textContent = "opção inválido";
+        opcaoOnline.parentElement.append(erro.cloneNode(true));
+    }*/
+
+    let opcao = document.querySelectorAll('input[name="opcao"]:checked');
+    if(opcao.length != 1){
+        ehValido = false;
+        erro.textContent = "opção inválido";
+        document.querySelector("#online").parentElement.append(erro.cloneNode(true));
     }
 
-    //validação radio 2
-    let horario = document.querySelectorAll('input[name="horario"]:checked');
-    if (horario.length != 1) {
-        valido = false;
-        erro.textContent = "horario inválido";
-        manha.parentElement.append(erro.cloneNode(true));
-    }
-
-    //validação checkbox
     let interesses = document.querySelectorAll('input[name="interesses"]:checked');
-    if (interesses.length == 0) {
-        valido = false;
+    if(interesses.length == 0){
+        ehValido = false;
         erro.textContent = "selecione pelo menos uma opção";
-        document.getElementById("meetups").parentElement.append(erro.cloneNode(true));
+        document.querySelector("#EAD").parentElement.append(erro.cloneNode(true));
     }
 
-    return valido;
+    return ehValido;
 }

a6ee0b0f9930b6df632193ca5e0b497dbb826aa0
YuriAugusto <yuri.komesu@uni9.edu.br>
2021-06-22 21:25:42 -0300

    Criação das validações com jQuery

--- workshop-js/js/script.js
+++ workshop-js/js/script.js
@@ -1,75 +1,156 @@
-let form;
+$(window).on("load", function () {//(window == página html)essa function faz com que o bloco seja executado assim que a página carregar
 
-function start() {
-    form = document.getElementById("formCadastro");
-    form.onsubmit = validar;
-    form.addEventListener("submit", validar);
-}  
-
-function limparErros() {
-    let arrayErros = document.getElementsByClassName("erro");
-    while (arrayErros.length > 0) {
-        let element = arrayErros[0];
-        element.remove();
-    }
-}
-
-function validarEmail(email) {
-    const re = /([a-z0-9_.]*\@([a-z]*)\.([a-z]{1,3})(\.[a-z]*)?)/g;
-    return re.test(email);
-}
-
-let validar = function () {
-    let valido = true;
-    /* let erro = $("<p>").addClass("erro"); */
-    let erro = document.createElement("p");
-    erro.className = "erro";
-    limparErros();
-
-    //validacao text
+    /* alert("nome") */;
     let nome = $("#nome");
-    if (nome.val() == "") {
-        valido = false;
-        erro.textContent = "Nome inválido";
-        nome.after(erro.cloneNode(true));
-    }
-
     let email = $("#email");
-    if (validarEmail(email.val().toLowerCase()) == false) {
-        valido = false;
-        erro.textContent = "E-mail inválido";
-        email.after(erro.cloneNode(true));
-    }
-
     let celular = $("#celular");
-    if (celular.val().length < 9) {
-        valido = false;
-        erro.textContent = "Celular inválido, digite ao menos 9 números";
-        celular.after(erro.cloneNode(true));
-    }
-
-    //validação select
     let curso = $("#curso");
-    if (curso.val() == "") {
-        valido = false;
-        erro.textContent = "Selecione um curso";
-        curso.after(erro.cloneNode(true));
+    let redireciona = $(".redirecionarPagina");
+   
+
+    //valida nome
+    // function validaNome() {
+    //     nome.on("input", function () {
+    //         if (nome.val() == "") {
+    //             //nome.after("<p>Nome inválido</p>").addClass(".pNome");//porque não é possível adicionar uma classe ao elemento criado desta forma?
+    //             $("<p style='color:#FC2727'>Nome inválido</p>").addClass("pNome").insertAfter(nome);
+    //         } else {
+    //             $(".pNome").remove();
+    //         }
+    //     });
+    // }
+
+    //valida nome
+    function validaNome() {
+        if (nome.val() == "") {
+            //nome.after("<p>Nome inválido</p>").addClass(".pNome");//porque não é possível adicionar uma classe ao elemento criado desta forma?
+            $("<p style='color:#FC2727'>Nome inválido</p>").addClass("pNome").insertAfter(nome);
+        } else {
+            $(".pNome").remove();
+        }
     }
 
-    //validação radio 
-    let periodo = $(".group input[type=radio]:checked").val();
-    if (periodo == null) {
-        valido = false;
-        erro.textContent = "Selecione um horário";
-        manha.parentElement.append(erro.cloneNode(true));
+    //valida email
+    // function validaEmail() {
+    //     email.on("input", function () {
+    //         if (email.val() == "") {
+    //             $("<p style='color:#FC2727'>Email inválido</p>").addClass("pEmail").insertAfter(email);
+    //         } else {
+    //             $(".pEmail").remove();
+    //         }
+    //     });
+    // }
+
+    //valida email
+    function validaEmail() {
+        if (email.val() == "") {
+            $("<p style='color:#FC2727'>Email inválido</p>").addClass("pEmail").insertAfter(email);
+        } else {
+            $(".pEmail").remove();
+        }
     }
 
-    //validação checkbox
-    let interesseCurso = $(".group input[type=checkbox]:checked").val();
-    if (interesseCurso == null) {
-        valido = false;
-        erro.textContent = "Selecione ao menos um interesse";
-        document.getElementById("meetups").parentElement.append(erro.cloneNode(true));
+    //valida celular
+    // function validaCelular() {
+    //     celular.on("input", function () {
+    //         if (celular.val() == "") {
+    //             $("<p style='color:#FC2727'>Celular inválido</p>").addClass("pCelular").insertAfter(celular);
+    //         } else {
+    //             $(".pCelular").remove();
+    //         }
+    //     });
+    // }
+
+    //valida celular
+    function validaCelular() {
+        if (celular.val() == "") {
+            $("<p style='color:#FC2727'>Celular inválido</p>").addClass("pCelular").insertAfter(celular);
+        } else {
+            $(".pCelular").remove();
+        }
     }
-    return valido;
-}
+
+    //valida select   
+    // function validaCurso() {
+    //     curso.on("click", function () {
+    //         if ($(curso).val() == 0) {
+    //             // $("#select_error").show();//mostra elemento oculto
+    //             $(".pSelect").remove();
+    //             $("<p style='color:#FC2727'>Você precisa selecionar um item</p>").addClass("pSelect").insertAfter(curso);//cria o elemento
+    //         } else {
+    //             // $("#select_error").hide();//oculta elemento de msg de erro
+    //             $(".pSelect").remove();//remove o elemento
+    //         }
+    //     });
+    // }
+
+    //valida select   
+    function validaCurso() {
+        if ($(curso).val() == 0) {
+            // $("#select_error").show();//mostra elemento oculto
+            $(".pSelect").remove();
+            $("<p style='color:#FC2727'>Você precisa selecionar um item</p>").addClass("pSelect").insertAfter(curso);//cria o elemento
+        } else {
+            // $("#select_error").hide();//oculta elemento de msg de erro
+            $(".pSelect").remove();//remove o elemento
+        }
+    }
+
+    //valida radio    
+    // function validaHorario() {
+    //     let inputHorario = $("#radioHorario").find("input");//aqui eu desci na árvore do DOM e acessei os radios através do seu elemento "pai"
+    //     titulo.on("click", function () {
+    //         if (inputHorario.is(":checked") == false) {
+    //             $(".pHorario").remove();
+    //             $("<p style='color:#FC2727'>É necessário selecionar um horário</p>").addClass("pHorario").insertAfter("#radioHorario");
+    //         } else {
+    //             $(".pHorario").remove();
+    //         }
+    //     });
+    // }
+
+    //valida radio    
+    function validaHorario() {
+        let inputHorario = $("#radioHorario").find("input");//aqui eu desci na árvore do DOM e acessei os radios através do seu elemento "pai"
+        if (inputHorario.is(":checked") == false) {
+            $(".pHorario").remove();
+            $("<p style='color:#FC2727'>É necessário selecionar um horário</p>").addClass("pHorario").insertAfter("#radioHorario");
+        } else {
+            $(".pHorario").remove();
+        }
+    }
+
+    //valida checkbox
+    // function validaInteresse(){
+    //     let inputInteresses = $("#interesses").find("input");
+    //     redireciona.on("click",function(){
+    //         if (inputInteresses.is(":checked") == false) {
+    //             $(".pInteresse").remove();
+    //             $("<p style='color:#FC2727'>É necessário selecionar ao menos um interesse</p>").addClass("pInteresse").insertAfter("#interesses");
+    //         }else { 
+    //             $(".pInteresse").remove();
+    //         }
+    //     });
+    // }
+ 
+    //valida checkbox
+    function validaInteresse() {
+        let inputInteresses = $("#interesses").find("input");
+        if (inputInteresses.is(":checked") == false) {
+            $(".pInteresse").remove();
+            $("<p style='color:#FC2727'>É necessário selecionar ao menos um interesse</p>").addClass("pInteresse").insertAfter("#interesses");
+        } else {
+            $(".pInteresse").remove();
+        }
+    }
+
+    //----------------------------------------
+    redireciona.on("click", function () {
+        validaNome();
+        validaEmail();
+        validaCelular();
+        validaCurso();
+        validaHorario();
+        validaInteresse();
+    });
+});

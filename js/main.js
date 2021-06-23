22b053d0e957649304f2149d2049f97efcbd6f74
HenriqueSRodrigues <henriquesilvarodrigues@gmail.com>
2021-06-23 13:57:34 -0300

    Meu ultimo commit

---
+++ js/main.js
@@ -0,0 +1,168 @@
+$(function ($) {
+  $("#celular").mask("(00)00000-0000");
+});
+$(window).on("load", function () {
+  let nome = $("#nome");
+  let sobrenome = $("#sobrenome");
+  let email = $("#email");
+  let celular = $("#celular");
+  let curso = $("#curso");
+  let enviar = $("#enviar");
+  let horario = $("#radioHorario");
+  let interesses = $("#checkboxInteresses");
+
+  let qtdValidacoesCorretas = 0;
+
+  function testaNome() {
+    if (nome.val() == "") {
+      $("#erroNome").text("Nome inválido!").show();
+    } else {
+      $("#erroNome").text("Nome inválido!").hide();
+      qtdValidacoesCorretas++;
+    }
+  }
+  function testaSobrenome() {
+    if (sobrenome.val() == "") {
+      $("#erroSobrenome").text("Sobrenome inválido!").show();
+    } else {
+      $("#erroSobrenome").text("Sobrenome inválido!").hide();
+      qtdValidacoesCorretas++;
+    }
+  }
+
+  function testaEmail() {
+    let = inputEmail=email.val();
+    const regexEmail = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+.[A-Z]{2,4}\b$/i;
+    if (!regexEmail.test(inputEmail)) {
+      $("#erroEmail").text("E-mail inválido!").show();
+    } else {
+      $("#erroEmail").text("E-mail inválido!").hide();
+      qtdValidacoesCorretas++;
+    }
+  }
+
+  function testaCelular() {
+    let = inputCelular = celular.val()
+    const regexCelular = /\([1-9]{2,})\d{4,}-\d{4}/i;
+    if (!regexCelular.test(inputCelular)) {
+      $("#erroCelular").text("Celular inválido!").show();
+    } else {
+      $("#erroCelular").text("Celular inválido!").hide();
+      qtdValidacoesCorretas++;
+    }
+  }
+
+  function testaCurso() {
+    if (curso.val() == "") {
+      $("#erroCurso").text("Curso inválido!").show();
+    } else {
+      $("#erroCurso").text("Curso inválido!").hide();
+      qtdValidacoesCorretas++;
+    }
+  }
+
+  function testaHorario() {
+    if (
+      !$("#manha:checked").is(":checked") &&
+      !$("#tarde:checked").is(":checked")
+    ) {
+      $("#erroHorario").text("Horário inválido!").show();
+    } else {
+      $("#erroHorario").text("Horário inválido!").hide();
+      qtdValidacoesCorretas++;
+    }
+  }
+  function testaInteresses() {
+    if (
+      !$("#ead:checked").is(":checked") &&
+      !$("#workshops:checked").is(":checked") &&
+      !$("#meetups:checked").is(":checked")
+    ) {
+      $("#erroInteresses").text("Selecione um campo").show();
+    } else {
+      $("#erroInteresses").text("Selecione um campo").hide();
+      qtdValidacoesCorretas++;
+    }
+  }
+  nome.focusout(function () {
+    if (nome.val() == "") {
+      $("#erroNome").text("Nome inválido!").show();
+    } else {
+      $("#erroNome").text("Nome inválido!").hide();
+    }
+  });
+  sobrenome.focusout(function () {
+    if (sobrenome.val() == "") {
+      $("#erroSobrenome").text("Sobrenome inválido!").show();
+    } else {
+      $("#erroSobrenome").text("Sobrenome inválido!").hide();
+    }
+  });
+  email.focusout(function () {
+    if (email.val() == "") {
+      $("#erroEmail").text("E-mail inválido!").show();
+    } else {
+      $("#erroEmail").text("E-mail inválido!").hide();
+    }
+  });
+  celular.focusout(function () {
+    if (celular.val() == "") {
+      $("#erroCelular").text("Celular inválido!").show();
+    } else {
+      $("#erroCelular").text("Celular inválido!").hide();
+    }
+  });
+  curso.click(function () {
+    if (curso.val() == "") {
+      $("#erroCurso").text("Curso inválido!").show();
+    } else {
+      $("#erroCurso").text("Curso inválido!").hide();
+    }
+  });
+  horario.click(function () {
+    if (
+      !$("#manha:checked").is(":checked") &&
+      !$("#tarde:checked").is(":checked")
+    ) {
+      $("#erroHorario").text("Horário inválido!").show();
+    } else {
+      $("#erroHorario").text("Horário inválido!").hide();
+    }
+  });
+  interesses.click(function () {
+    if (
+      $("#ead:checked").is(":checked") ||
+      $("#workshops:checked").is(":checked") ||
+      $("#meetups:checked").is(":checked")
+    ) {
+      $("#erroInteresses").text("Selecione um campo").hide();
+    } else {
+      $("#erroInteresses").text("Selecione um campo").show();
+    }
+  });
+
+  enviar.click(function () {
+    testaNome();
+    testaSobrenome();
+    testaEmail();
+    testaCelular();
+    testaCurso();
+    testaHorario();
+    testaInteresses();
+    if (qtdValidacoesCorretas > 6) {
+      window.location.href = "enviado.html";
+    } else {
+      $("#erroEnviar").text("Preencha todo o Formulário").show();
+    }
+    console.log("qtdValidacoesCorretas " + qtdValidacoesCorretas);
+    qtdValidacoesCorretas = 0;
+  });
+
+  nome.click(testaNome);
+  sobrenome.click(testaSobrenome);
+  email.click(testaEmail);
+  celular.click(testaCelular);
+  curso.select(testaCurso);
+  enviar.select(testaHorario);
+  enviar.click(testaInteresses);
+});
